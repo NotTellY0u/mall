74329737d291b2ebedad21717322e755ca9dcb35
yfl <yangfulin123@qq.com>
2020-12-14 21:45:29 +0800

    新增gateway

---
+++ mall-gateway/src/main/java/me/lin/mall/mallgateway/MallGatewayApplication.java
@@ -0,0 +1,24 @@
+package me.lin.mall.mallgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import javax.sql.DataSource;
+
+/**
+ * @author yangfulin
+ * 1.开启服务注册发现
+ * (配置nacos的注册中心地址)
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
