b7df070c01628e97605b4119ea5628eacf5a639e
yfl <yangfulin123@qq.com>
2020-12-20 13:50:21 +0800

    新增商品服务递归树形结构数据获取

--- mall-product/src/main/java/me/lin/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/me/lin/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,9 @@
 package me.lin.mall.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -16,6 +19,9 @@ import me.lin.mall.product.service.CategoryService;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
+    @Autowired
+    CategoryDao categoryDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
@@ -24,6 +30,14 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1.查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        //2.组装父子的树形结构
+        return entities;
     }
 
 }
