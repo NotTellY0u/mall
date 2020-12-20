fa1c500b7319398f0b4347eba1bf942689caa772
yfl <yangfulin123@qq.com>
2020-12-20 15:49:07 +0800

    新增商品服务递归树形结构数据获取

--- mall-product/src/main/java/me/lin/mall/product/entity/CategoryEntity.java
+++ mall-product/src/main/java/me/lin/mall/product/entity/CategoryEntity.java
@@ -1,10 +1,13 @@
 package me.lin.mall.product.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
+
 import lombok.Data;
 
 /**
@@ -57,4 +60,6 @@ public class CategoryEntity implements Serializable {
 	 */
 	private Integer productCount;
 
+	@TableField(exist = false)
+	private List<CategoryEntity> children;
 }
