7947876991b4417edf785d8134549c932818d4dc
lin <yangfulin123@qq.com>
2021-01-06 11:06:26 +0800

    新增oss功能

---
+++ mall-third-party/src/main/java/me/lin/mall/thirdparty/MallThirdPartyApplication.java
@@ -0,0 +1,13 @@
+package me.lin.mall.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallThirdPartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallThirdPartyApplication.class, args);
+    }
+
+}
---
+++ mall-third-party/src/test/java/me/lin/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lin.mall.thirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
