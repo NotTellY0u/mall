7a6323a26449ed33316492633359ba5cdfc0fe89
lin <yangfulin123@qq.com>
2021-01-12 17:26:59 +0800

    mybatis-plus分页插件，商品模糊查询

--- mall-product/src/main/java/me/lin/mall/product/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/me/lin/mall/product/service/impl/BrandServiceImpl.java
@@ -1,7 +1,10 @@
 package me.lin.mall.product.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -18,9 +21,16 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        // 1.获取key
+        String key = (String) params.get("key");
+        QueryWrapper<BrandEntity> queryWrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.eq("brand_id", key).or().like("name", key);
+        }
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
