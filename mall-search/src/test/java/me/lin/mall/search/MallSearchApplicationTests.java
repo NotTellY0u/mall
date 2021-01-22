0fc46292351e4f722af3f3e832528fd866857a22
lin <yangfulin123@qq.com>
2021-01-22 17:15:20 +0800

    新增mall-search模块

---
+++ mall-search/src/test/java/me/lin/mall/search/MallSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lin.mall.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
