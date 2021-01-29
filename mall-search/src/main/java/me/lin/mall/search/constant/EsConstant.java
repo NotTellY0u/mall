775df27432cce60e0756f151dd549f5428ca14da
lin <yangfulin123@qq.com>
2021-01-29 10:39:35 +0800

    远程上架

---
+++ mall-search/src/main/java/me/lin/mall/search/constant/EsConstant.java
@@ -0,0 +1,11 @@
+package me.lin.mall.search.constant;
+
+/**
+ * @Author Fibonacci
+ * @create: 2021-01-28 09:50
+ * @Version 1.0
+ */
+public class EsConstant {
+    public static final String PRODUCT_INDEX = "product"; //sku数据在es中的索引
+
+}
