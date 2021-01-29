4cc35333400bd60732b30d87e5bb65a746f952db
lin <yangfulin123@qq.com>
2021-01-29 13:55:23 +0800

    远程上架

--- mall-search/src/main/java/me/lin/mall/search/service/impl/ProductSaveServiceImpl.java
+++ mall-search/src/main/java/me/lin/mall/search/service/impl/ProductSaveServiceImpl.java
@@ -1,7 +1,15 @@
 package me.lin.mall.search.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import me.lin.mall.common.to.es.SkuEsModel;
+import me.lin.mall.search.constant.EsConstant;
 import me.lin.mall.search.service.ProductSaveService;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +21,11 @@ import java.util.List;
  */
 @Service
 public class ProductSaveServiceImpl implements ProductSaveService {
+
+    @Qualifier("esRestClient")
+    @Autowired
+    RestHighLevelClient restHighLevelClient;
+
     /**
      * 保存上架状态
      *
@@ -20,6 +33,23 @@ public class ProductSaveServiceImpl implements ProductSaveService {
      */
     @Override
     public void productStatusUp(List<SkuEsModel> skuEsModels) {
+
+        //保存到es
+        // 1.给es中建立索引,product，建立映射关系
+
+        // 2.给es中保存这些数据
+        //BulkRequest\
+        BulkRequest bulkRequest = new BulkRequest();
+        for (SkuEsModel skuEsModel : skuEsModels) {
+            // 构造保存请求
+            IndexRequest indexRequest = new IndexRequest(EsConstant.PRODUCT_INDEX);
+            indexRequest.id(String.valueOf(skuEsModel.getSkuId()));
+            String s = JSON.toJSONString(skuEsModel);
+            indexRequest.source(s, XContentType.JSON);
+            bulkRequest.add(indexRequest);
+
+
+        }
 
     }
 }
