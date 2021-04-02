32a039e52461e7bf71f59183b22c1b64cb0b628f
yfl <yangfulin123@qq.com>
2021-04-02 14:17:49 +0800

    修改页面登陆信息显示

--- mall-search/src/main/java/me/lin/mall/search/feign/ProductFeignService.java
+++ mall-search/src/main/java/me/lin/mall/search/feign/ProductFeignService.java
@@ -4,7 +4,6 @@ import me.lin.mall.common.utils.R;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -18,9 +17,9 @@ import java.util.List;
 public interface ProductFeignService {
 
     @GetMapping("/product/attr/info/{attrId}")
-    public R attrInfo(@PathVariable("attrId") Long attrId);
+    R attrInfo(@PathVariable("attrId") Long attrId);
 
     @GetMapping("/product/brand/infos")
-    public R brandInfo(@RequestParam("brandIds") List<Long> brandIds);
+    R brandInfo(@RequestParam("brandIds") List<Long> brandIds);
 
 }
