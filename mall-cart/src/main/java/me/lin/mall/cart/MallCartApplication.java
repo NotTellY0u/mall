e428741d72d8406240fc9d59a2a9aee7f1a261ac
yfl <yangfulin123@qq.com>
2021-04-12 11:11:01 +0800

    新增购物车

---
+++ mall-cart/src/main/java/me/lin/mall/cart/MallCartApplication.java
@@ -0,0 +1,13 @@
+package me.lin.mall.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallCartApplication.class, args);
+    }
+
+}
---
+++ mall-cart/src/test/java/me/lin/mall/cart/MallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lin.mall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
