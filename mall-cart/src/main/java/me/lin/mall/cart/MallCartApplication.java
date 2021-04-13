edd5003a56724d5fba4f98101318ef7257726211
yfl <yangfulin123@qq.com>
2021-04-13 09:30:24 +0800

    创建购物车模块

--- mall-cart/src/main/java/me/lin/mall/cart/MallCartApplication.java
+++ mall-cart/src/main/java/me/lin/mall/cart/MallCartApplication.java
@@ -2,8 +2,13 @@ package me.lin.mall.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MallCartApplication {
 
     public static void main(String[] args) {
