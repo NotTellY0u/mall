1a1176e36351c6512bd05c1ad44f227ebc719a65
lin <foxabc@gmail.com>
2021-11-29 19:51:33 +0800

    环境迁移

--- mall-auth-server/src/main/java/me/lin/mall/auth/MallAuthServerApplication.java
+++ mall-auth-server/src/main/java/me/lin/mall/auth/MallAuthServerApplication.java
@@ -2,6 +2,7 @@ package me.lin.mall.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
