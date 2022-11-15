a22bc6f9376ee37e68c2bece6b183cb0b94c3a44
lin <foxabc@gmail.com>
2022-11-15 19:59:39 +0800

    add rabbitmq

--- mall-order/src/main/java/me/lin/mall/order/config/MyMQConfig.java
+++ mall-order/src/main/java/me/lin/mall/order/config/MyMQConfig.java
@@ -4,6 +4,8 @@ import com.rabbitmq.client.Channel;
 import me.lin.mall.order.entity.OrderEntity;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,11 @@ import java.util.HashMap;
 
 @Configuration
 public class MyMQConfig {
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
 
     @RabbitListener(queues = "order.release.order.queue")
     public void listener(OrderEntity entity, Channel channel, Message message){
