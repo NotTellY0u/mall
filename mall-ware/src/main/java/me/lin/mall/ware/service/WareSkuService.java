f291294c2d95786017d9a27ec1d2bebae97c23fb
lin <bugaosuni891@gmail.com>
2023-03-12 21:33:48 +0800

    test auto unlock

--- mall-ware/src/main/java/me/lin/mall/ware/service/WareSkuService.java
+++ mall-ware/src/main/java/me/lin/mall/ware/service/WareSkuService.java
@@ -1,6 +1,7 @@
 package me.lin.mall.ware.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import me.lin.mall.common.to.mq.OrderTo;
 import me.lin.mall.common.to.mq.StockLockedTo;
 import me.lin.mall.common.utils.PageUtils;
 import me.lin.mall.ware.entity.WareSkuEntity;
@@ -10,33 +11,23 @@ import me.lin.mall.ware.vo.WareSkuLockVo;
 import java.util.List;
 import java.util.Map;
 
+
 /**
  * 商品库存
  *
- * @author Fibonacci
- * @email bugaosuni@gmail.com
- * @date 2020-12-11 22:59:58
+ * @author Ethan
+ * @email hongshengmo@163.com
+ * @date 2020-05-27 23:15:25
  */
 public interface WareSkuService extends IService<WareSkuEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
 
-
     void addStock(Long skuId, Long wareId, Integer skuNum);
 
-    /**
-     * 判断是否有库存
-     * @param skuIds skuID
-     * @return 返回信息
-     */
-    List<SkuHasStockVo> getSkusHasStock(List<Long> skuIds);
+    List<SkuHasStockVo> getSkuHasStocks(List<Long> ids);
 
-    /**
-     * 为某个订单锁定库存
-     * @param vo 订单信息
-     * @return 结果
-     */
-    boolean orderLockStock(WareSkuLockVo vo);
+    Boolean orderLockStock(WareSkuLockVo lockVo);
 
     void unlock(StockLockedTo stockLockedTo);
 
