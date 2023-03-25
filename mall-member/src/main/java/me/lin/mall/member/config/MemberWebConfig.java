cce97e0f5df41d373a652895238b67c8dfdf6476
lin <bugaosuni891@gmail.com>
2023-03-25 11:07:53 +0800

    add payment

---
+++ mall-member/src/main/java/me/lin/mall/member/config/MemberWebConfig.java
@@ -0,0 +1,9 @@
+package me.lin.mall.member.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MemberWebConfig implements WebMvcConfigurer {
+
+}
